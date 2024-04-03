f69ddaa9bafa102616d9b7537be01953eccf7098
Yarovaya Elizaveta <elisabetatchaikovsckaya@ya.ru>
2024-04-03 20:48:38 +0300

    мяу

--- src/widgets/MainPage/MainPage.tsx
+++ src/widgets/MainPage/MainPage.tsx
@@ -1,24 +1,19 @@
 import styles from './MainPage.module.scss'
 import Container from "../../shared/ui/Conteiner/Container.tsx";
-import image from '../../app/assets/image/vvvvvvvvvvvzzzzzzzz.png'
 import {Button} from "../../shared/ui/button/Button.tsx";
 
 export default function MainPage(){
     return(
         <Container rootClassName={styles.root} className={styles.root_wrapper}>
-            <div className={styles.root_image}>
-                <img src = {image} />
-            </div>
             <div className={styles.root_info}>
-                    <h1>Tasks -</h1>
-                    <p >приложение для ведения заметок и отчета задач а еще мяу мяу мяу</p>
+                <div className={styles.root_button_container}><h1>Organize your work and life, finally.</h1></div>
+
+                    <p>Become focused, organized, and calm with Todoist. The world's #1 task manager and to-do list app.</p>
                 <div className={styles.root_button_container}>
                 <Button type={'primary'} buttonSize={'default'}>Start</Button>
                 </div>
             </div>
-            <div className={styles.root_block}>
 
-            </div>
         </Container>
     )
 }
