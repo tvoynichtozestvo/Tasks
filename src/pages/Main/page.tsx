1ec05f55012c022b99e741fdd8dd707582eb67b7
Maxim <maxkryu2004@gmail.com>
2024-04-06 17:07:30 +0300

    доработка главной страницы

--- src/pages/Main/page.tsx
+++ src/pages/Main/page.tsx
@@ -1,5 +1,13 @@
 import MainPage from "../../widgets/MainPage/MainPage.tsx";
+import Mainpageimg from "../../widgets/MainpageImg/Mainpageimg.tsx";
+import Mainpageimgsecond from "../../widgets/MainpageImgsecond/Mainpageimg.tsx";
 
 export default function Home() {
-    return <MainPage></MainPage>
+    return(
+        <>
+        <MainPage/>
+        <Mainpageimg/>
+         <Mainpageimgsecond/>
+        </>
+    )
 }
---
+++ src/widgets/MainpageImgsecond/Mainpageimg.tsx
@@ -0,0 +1,25 @@
+import styles from './MainPageImg.module.scss'
+import image from '../../app/assets/image/screenshot2.png'
+import Container from "../../shared/ui/Conteiner/Container.tsx";
+import {Button} from "../../shared/ui/button/Button.tsx";
+
+
+export default function Mainpageimgsecond(){
+
+    return(
+        <Container rootClassName={styles.root} className={styles.root_wrapper}>
+            <div className={styles.root_info}>
+                <h2>
+                    Organize your life
+                </h2>
+                <p>
+                    Whether you need to complete a task for work or school, Tasks is here to help you manage all your tasks.
+                </p>
+                <div className={styles.root_info_button_container}>
+                <Button type={'primary'}>More</Button>
+                </div>
+            </div>
+            <img src={image}/>
+        </Container>
+    )
+}
