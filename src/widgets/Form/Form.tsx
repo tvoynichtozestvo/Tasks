cc03ced3e7c3321f65d8759c39725e2f48bbb407
Maxim <maxkryu2004@gmail.com>
2024-04-09 15:36:08 +0300

    Ураааа мы умеем регистрироваться

--- src/widgets/Form/Form.tsx
+++ src/widgets/Form/Form.tsx
@@ -1,4 +1,3 @@
-
 import AuthForm from "../../shared/ui/FormData/FormData.tsx";
 import styles from "./Form.module.scss";
 import ContainerToForm from "../../shared/ui/ConteinerToForm/Container.tsx";
@@ -7,6 +6,7 @@ import {Link} from "react-router-dom";
 export default function Form(){
     return(
         <ContainerToForm rootClassName={styles.root} className={styles.root__wrapper}>
+            <div>
             <AuthForm
                 title={'Login'}
                 button={'Login'}
@@ -15,8 +15,13 @@ export default function Form(){
                     { id: 'password', placeholder: 'Password', type: 'primary', inputSize: 'medium' }
                 ]}>
             </AuthForm>
+            </div>
+
+            <div>
             <Link to={'https://www.youtube.com/watch?v=wv969vgqwDw'}>Forgot password?</Link>
             <Link to={'/register'}>register</Link>
+            </div>
+
         </ContainerToForm>
     )
 }
--- src/widgets/FormRegister/FormRegister.tsx
+++ src/widgets/FormRegister/FormRegister.tsx
@@ -1,10 +1,12 @@
 import AuthForm from "../../shared/ui/FormData/FormData.tsx";
 import styles from "./Form.module.scss";
 import ContainerToForm from "../../shared/ui/ConteinerToForm/Container.tsx";
+import {Link} from "react-router-dom";
 
 export default function FormRegister(){
     return(
         <ContainerToForm rootClassName={styles.root} className={styles.root__wrapper}>
+            <div>
             <AuthForm
                 title={'Register'}
                 button={'Register'}
@@ -15,6 +17,12 @@ export default function FormRegister(){
                     { id: 'password', placeholder: 'Confirm password', type: 'primary', inputSize: 'medium' }
                 ]}>
             </AuthForm>
+            </div>
+            <div>
+                <Link to={'https://www.youtube.com/watch?v=wv969vgqwDw'}>Forgot password?</Link>
+                <Link to={'/login'}>login</Link>
+            </div>
+
         </ContainerToForm>
     )
 }
