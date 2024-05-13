d8138853c0f9fedb5f5b4955ab9f5b16e7368c22
Maxim <maxkryu2004@gmail.com>
2024-05-13 12:08:13 +0300

    небольшие изменения в settings и addModal

--- src/widgets/Settings/Settings.tsx
+++ src/widgets/Settings/Settings.tsx
@@ -72,86 +72,87 @@ export default function Settings(){
             <div>
                 <h2>Settings</h2>
             </div>
-            <div className={styles.root_navContaier}>
-                <div onClick={setProfile} className={styles.root_navContaier_navItems} id={'profile'} style={{color: '#6964E2', textDecoration: 'underline'}}>Profile</div>
-                <div onClick={setTheme} className={styles.root_navContaier_navItems} id={'theme'}>Theme</div>
-                <div onClick={setOther} className={styles.root_navContaier_navItems} id={'other'}>Other</div>
-            </div>
-            <div className={styles.root_mainContainer}>
-            {selectedTab === 'Profile' &&
-             <div>
-                 <div className={styles.root_profileIcontContainer}>
-                     <img src={icon} alt=""/>
-                     <Button type={'primary'} buttonSize={'small'}>change</Button>
-                     <Button type={'secondary'} buttonSize={'small'}>delete</Button>
-                 </div>
-
-                 <div className={styles.root_profileInfoContainer}>
-                     <div className={styles.root_profileInfoContainer_profileInfoItems}>
-                         <h4>Login:</h4>
-                         <TextInput type={'primary'}/>
-                     </div>
-                     <div className={styles.root_profileInfoContainer_profileInfoItems} >
-                         <h4>Email: </h4>
-                         <span style={{fontSize: '20px', marginBottom: '6px', marginRight: '5px'}}>memimemamomy@gmail.com</span>
-                         <img src={pencilImage} alt=""/>
-                     </div>
-                     <div className={styles.root_profileInfoContainer_profileInfoItems}>
-                         <h4>Password</h4>
+            <div className={styles.root_borderUraa}>
+                <div className={styles.root_navContaier}>
+                    <div onClick={setProfile} className={styles.root_navContaier_navItems} id={'profile'} style={{color: '#6964E2', textDecoration: 'underline'}}>Profile</div>
+                    <div onClick={setTheme} className={styles.root_navContaier_navItems} id={'theme'}>Theme</div>
+                    <div onClick={setOther} className={styles.root_navContaier_navItems} id={'other'}>Other</div>
+                </div>
+                <div className={styles.root_mainContainer}>
+                {selectedTab === 'Profile' &&
+                 <div>
+                     <div className={styles.root_profileIcontContainer}>
+                         <img src={icon} alt=""/>
                          <Button type={'primary'} buttonSize={'small'}>change</Button>
-                     </div>
-                     <div className={styles.root_profileInfoContainer_profileInfoItems}>
-                         <Button type={'secondary'} buttonSize={'small'}>delete account</Button>
+                         <Button type={'secondary'} buttonSize={'small'}>delete</Button>
                      </div>
 
-                 </div>
+                     <div className={styles.root_profileInfoContainer}>
+                         <div className={styles.root_profileInfoContainer_profileInfoItems}>
+                             <h4>Login:</h4>
+                             <TextInput type={'primary'}/>
+                         </div>
+                         <div className={styles.root_profileInfoContainer_profileInfoItems} >
+                             <h4>Email: </h4>
+                             <span style={{fontSize: '20px', marginBottom: '6px', marginRight: '5px'}}>memimemamomy@gmail.com</span>
+                             <img src={pencilImage} alt=""/>
+                         </div>
+                         <div className={styles.root_profileInfoContainer_profileInfoItems}>
+                             <h4>Password</h4>
+                             <Button type={'primary'} buttonSize={'small'}>change</Button>
+                         </div>
+                         <div className={styles.root_profileInfoContainer_profileInfoItems}>
+                             <Button type={'secondary'} buttonSize={'small'}>delete account</Button>
+                         </div>
 
-            </div>}
-            {selectedTab === 'Theme' &&
-                <div className={styles.root_themeContainer}>
-                    <div className={styles.root_themeContainer_themeWrapper}>
-                        <div>
-                            <h2>Dark theme:</h2>
-                            <div className={styles.root_themeContainer_themeWrapper_themeContaienr}>
-                                <div style={{background: 'black'}}></div>
-                                <div style={{background: '#6964E2'}}></div>
-                            </div>
-                        </div>
-                        <div className={styles.root_themeContainer_applyContainer}>
-                            <div><img src={eyeImage} alt=""/></div>
-                            <div><Button type={'primary'} buttonSize={'default'}>Change</Button></div>
-                        </div>
-                    </div>
+                     </div>
 
-                    <div className={styles.root_themeContainer_themeWrapper}>
-                        <div>
-                            <h2>Light theme:</h2>
-                            <div className={styles.root_themeContainer_themeWrapper_themeContaienr}>
-                                <div style={{background: 'white'}}></div>
-                                <div style={{background: '#6964E2'}}></div>
-                            </div>
-                        </div>
-                        <div className={styles.root_themeContainer_applyContainer}>
-                            <div><img src={eyeImage} alt=""/></div>
-                            <div><Button type={'primary'} buttonSize={'default'}>Change</Button></div>
-                        </div>
-                    </div>
-
-            </div>}
-            {selectedTab === 'Other' &&
-                <div className={styles.root_other}>
-                    <div><h2>Language</h2></div>
-                    <select>
-                        <option>English</option>
-                        <option>Russia</option>
-                    </select>
                 </div>}
-            </div>
-            <div className={styles.root_settingsButton}>
-                <Button type={'primary'} buttonSize={'small'}>save</Button>
-                <Button type={'secondary'} buttonSize={'small'}>cancel</Button>
-            </div>
+                {selectedTab === 'Theme' &&
+                    <div className={styles.root_themeContainer}>
+                        <div className={styles.root_themeContainer_themeWrapper}>
+                            <div>
+                                <h2>Dark theme:</h2>
+                                <div className={styles.root_themeContainer_themeWrapper_themeContaienr}>
+                                    <div style={{background: 'black'}}></div>
+                                    <div style={{background: '#6964E2'}}></div>
+                                </div>
+                            </div>
+                            <div className={styles.root_themeContainer_applyContainer}>
+                                <div><img src={eyeImage} alt=""/></div>
+                                <div><Button type={'primary'} buttonSize={'default'}>Change</Button></div>
+                            </div>
+                        </div>
 
+                        <div className={styles.root_themeContainer_themeWrapper}>
+                            <div>
+                                <h2>Light theme:</h2>
+                                <div className={styles.root_themeContainer_themeWrapper_themeContaienr}>
+                                    <div style={{background: 'white'}}></div>
+                                    <div style={{background: '#6964E2'}}></div>
+                                </div>
+                            </div>
+                            <div className={styles.root_themeContainer_applyContainer}>
+                                <div><img src={eyeImage} alt=""/></div>
+                                <div><Button type={'primary'} buttonSize={'default'}>Change</Button></div>
+                            </div>
+                        </div>
+
+                </div>}
+                {selectedTab === 'Other' &&
+                    <div className={styles.root_other}>
+                        <div><h2>Language</h2></div>
+                        <select>
+                            <option>English</option>
+                            <option>Russia</option>
+                        </select>
+                    </div>}
+                </div>
+                <div className={styles.root_settingsButton}>
+                    <Button type={'primary'} buttonSize={'small'}>save</Button>
+                    <Button type={'secondary'} buttonSize={'small'}>cancel</Button>
+                </div>
+            </div>
         </Container>
     )
 }
